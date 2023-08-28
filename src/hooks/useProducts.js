ce6dc1e1c26a3b732f02fa264621da1d360258b5
ColoradaGreis <graciana.baratti@gmail.com>
2023-08-28 11:50:33 -0300

    [fix] update api imports

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,37 +1,32 @@
-import { useState, useEffect } from 'react';
-import DspApi from '@/lib/api';
-
-
+import { useState, useEffect } from 'react'
+import { listProducts } from '@/pages/api/product'
 
 const useGetProducts = (params, cachekey) => {
-  const [products, setProducts] = useState(null);
-  const [meta, setMeta] = useState(null);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState(null)
+  const [meta, setMeta] = useState(null)
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
 
   const fetchData = () => {
     setLoading(true)
-    DspApi.listProducts(params.limit, params.page, params.search).then(
+    listProducts(params.limit, params.page, params.search).then(
       (response) => {
         setProducts(response.data.data)
         setMeta(response.data.meta)
       }
     ).catch((err) => {
-      setError(err);
+      setError(err)
     }).finally(() => {
-      setLoading(false);
-    });
+      setLoading(false)
+    })
   }
 
-
   useEffect(() => {
-    fetchData();
-  }, [params, cachekey]);
+    fetchData()
+  }, [params, cachekey])
 
   // custom hook returns value
-  return { products, meta, error, loading };
-
-
+  return { products, meta, error, loading }
 }
 
-export default useGetProducts;
+export default useGetProducts
--- src/hooks/useWarehouses.js
+++ src/hooks/useWarehouses.js
@@ -1,37 +1,32 @@
-import { useState, useEffect } from 'react';
-import DspApi from '@/lib/api';
-
-
+import { useState, useEffect } from 'react'
+import { listWarehouses } from '@/pages/api/warehouse'
 
 const useGetWarehouses = (params, cachekey) => {
-  const [warehouses, setWarehouses] = useState(null);
-  const [meta, setMeta] = useState(null);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(true);
+  const [warehouses, setWarehouses] = useState(null)
+  const [meta, setMeta] = useState(null)
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
 
   const fetchData = () => {
     setLoading(true)
-    DspApi.listWarehouses(params.limit, params.page).then(
+    listWarehouses(params.limit, params.page).then(
       (response) => {
         setWarehouses(response.data.data)
         setMeta(response.data.meta)
       }
     ).catch((err) => {
-      setError(err);
+      setError(err)
     }).finally(() => {
-      setLoading(false);
-    });
+      setLoading(false)
+    })
   }
 
-
   useEffect(() => {
-    fetchData();
-  }, [params, cachekey]);
+    fetchData()
+  }, [params, cachekey])
 
   // custom hook returns value
-  return { warehouses, meta, error, loading };
-
-
+  return { warehouses, meta, error, loading }
 }
 
-export default useGetWarehouses;
+export default useGetWarehouses
