1358bf7e8a4858d263966732e9e3aecc6aa4bba0
ColoradaGreis <graciana.baratti@gmail.com>
2023-08-29 18:16:27 -0300

    [feat] add fetchProducts by id

--- src/pages/restock/stepTwo.js
+++ src/pages/restock/stepTwo.js
@@ -1,39 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { getInventoryByStore, getLayout } from '../api/store';
+import React, { useEffect, useState } from 'react'
+import { getInventoryByStore } from '../api/store'
+import { getLayout } from '../api/layout'
+import { getProduct } from '../api/product'
 
-function StepTwo({ selectedStore, currentStep }) {
-  const [inventory, setInventory] = useState([]);
-  const [layout, setLayout] = useState([]);
+function StepTwo ({ selectedStore, currentStep }) {
+  const [inventory, setInventory] = useState([])
+  const [layout, setLayout] = useState([])
+  const [products, setProducts] = useState([])
 
-  const shouldFetchData = selectedStore && currentStep === 2;
+  const shouldFetchData = selectedStore && currentStep === 2
 
   useEffect(() => {
     if (shouldFetchData) {
-      fetchInventoryAndLayout(selectedStore.external_id);
+      fetchInventoryAndLayout(selectedStore.external_id)
     }
-  }, [shouldFetchData, selectedStore]);
+  }, [shouldFetchData, selectedStore])
 
-  async function fetchInventoryAndLayout(storeId) {
+  async function fetchInventoryAndLayout (storeId) {
     try {
-      const inventoryResponse = await getInventoryByStore(storeId);
-      console.log('la respuesta del coso');
-      console.log(inventoryResponse);
+      const inventoryResponse = await getInventoryByStore(storeId)
+      //   console.log('la respuesta del coso')
+      //   console.log(inventoryResponse)
       if (inventoryResponse.data) {
-        setInventory(inventoryResponse.data);
-        const layoutId = inventoryResponse.data.layoutId;
-        //const layoutResponse = await getLayout(layoutId);
-        //setLayout(layoutResponse.data);
+        setInventory(inventoryResponse.data)
+        const layoutId = selectedStore.layout_id
+        const layoutResponse = await getLayout(layoutId)
+        setLayout(layoutResponse.data)
+        if (layoutResponse.data) {
+          fetchProducts(inventoryResponse.data)
+        }
       }
     } catch (error) {
-      console.error('Error fetching inventory or layout:', error);
+      console.error('Error fetching inventory or layout:', error)
+    }
+  }
+  async function fetchProducts (inventory) {
+    console.log('entré al fetchProducts')
+    try {
+      const productsResponse = await Promise.all(
+        inventory.products.map(async (item) => {
+          const productResponse = await getProduct(item.productId)
+          return productResponse.data
+        })
+      )
+      setProducts(productsResponse)
+    } catch (error) {
+      console.error('Error fetching products:', error)
     }
   }
 
   return (
     <div>
-    <pre>{JSON.stringify(inventory, null, 2)}</pre>
+      <pre>{JSON.stringify(inventory, null, 2)}</pre>
     </div>
-  );
+  )
 }
 
-export default StepTwo;
+export default StepTwo
