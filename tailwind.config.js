f5624bb5ec90eae777a9443450568eb707379d1a
Gary Velasquez <gary.velasquez@cencosud.cl>
2023-08-29 17:08:14 -0400

    [fix] update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,28 +11,29 @@ module.exports = {
     fontFamily: {
       'albert-sans': ['Albert Sans', 'sans-serif']
     },
-    colors: {
-      'd-title-purple': '#6b648d',
-      'd-dark-dark-purple': '#462b99',
-      'd-soft-soft-purple': '#d5d3fb',
-      'd-dark-purple': '#504695',
-      'd-purple': '#7e4bff',
-      'd-soft-purple': '#8c7be5',
-      'd-strong-green': '#ddff4c',
-      'd-green': '#e7ff6f',
-      'd-soft-green': '#d8dcb0',
-      'd-dark-gray': '#e8e8d8',
-      'd-gray': '#e5e5e3',
-      'd-white': '#ffffff'
-
-    },
     extend: {
+      colors: {
+        // Tus colores personalizados
+        'd-title-purple': '#6b648d',
+        'd-dark-dark-purple': '#462b99',
+        'd-soft-soft-purple': '#d5d3fb',
+        'd-dark-purple': '#504695',
+        'd-purple': '#7e4bff',
+        'd-soft-purple': '#8c7be5',
+        'd-strong-green': '#ddff4c',
+        'd-green': '#e7ff6f',
+        'd-soft-green': '#d8dcb0',
+        'd-dark-gray': '#e8e8d8',
+        'd-gray': '#e5e5e3',
+        'd-white': '#ffffff',
+        // ...
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       }
-    }
+    },
   },
   plugins: [require('daisyui')]
-}
+};
