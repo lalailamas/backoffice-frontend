5dbafa8f39d2513291d0faa07c80161222e7137b
lalailamas <ale.cordoba.p@gmail.com>
2023-09-07 18:20:22 -0300

    [fix] modify form create user- react hook form

--- src/pages/users/create.js
+++ src/pages/users/create.js
@@ -1,31 +1,28 @@
 import { useState } from 'react'
-import { createUser } from '../api/user'
 import InsideLayout from '@/components/admin/layouts/inside'
+import { useForm, Controller } from 'react-hook-form'
 import { useRouter } from 'next/router'
+import { createUser } from '../api/user'
 
 function CreateUserForm () {
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-    first_name: '',
-    first_lastname: '',
-    second_lastname: '',
-    role: ''
+  const { control, handleSubmit, setError, formState, watch } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      first_name: '',
+      first_lastname: '',
+      second_lastname: '',
+      role: ''
+    }
   })
-
-  const [error, setError] = useState(null)
   const [successMessage, setSuccessMessage] = useState('')
   const router = useRouter()
 
-  const handleCreateAccount = () => {
-    if (!user.email || !user.password || user.password.length < 6) {
-      setError('Complete todos los campos correctamente')
-      return
-    }
-
-    createUser(user)
+  const onSubmit = handleSubmit((data) => {
+    console.log(data, 'aqui esta la data')
+    createUser(data)
       .then((response) => {
-        console.log(response, 'respuesta de createUser')
+        console.log(response, 'entre al createUser')
         setSuccessMessage('Usuario creado exitosamente')
         setTimeout(() => {
           router.push('/users')
@@ -33,8 +30,9 @@ function CreateUserForm () {
       })
       .catch((error) => {
         console.error('Error al crear la cuenta:', error)
+        setError('Error al crear la cuenta. Por favor, inténtelo de nuevo.')
       })
-  }
+  })
 
   return (
     <>
@@ -43,94 +41,223 @@ function CreateUserForm () {
         <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center'>
           <div className='bg-white px-6 py-3 w-full'>
             <h1 className='text-d-dark-dark-purple text-2xl font-bold pb-4'>Crear cuenta</h1>
-            <div className='pb-4'>
-              <select
-                defaultValue='read'
-                className='select select-sm select-bordered rounded-full w-full md:max-w-xs'
-                onChange={(e) => setUser({ ...user, role: e.target.value })}
-                required
+            <form onSubmit={onSubmit}>
+              <div className='pb-4'>
+                <label className='label'>Tipo de perfil</label>
+                <Controller
+                  name='role'
+                  control={control}
+                  defaultValue='read'
+                  rules={{ required: 'Seleccione un tipo de perfil' }}
+                  render={({ field }) => (
+                    <select
+                      {...field}
+                      className='select select-sm select-bordered rounded-full w-full md:max-w-xs'
+                    >
+                      <option value='admin'>Admin</option>
+                      <option value='restock'>Reponedor</option>
+                      <option value='read'>Lector</option>
+                    </select>
+                  )}
+                />
+                {formState.errors.role && (
+                  <p className='text-error'>{formState.errors.role.message}</p>
+                )}
+              </div>
+
+              <div className='pb-4'>
+                <label className='label'>Nombre</label>
+                <Controller
+                  name='first_name'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'El nombre es requerido'
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='text'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Nombre'
+                      />
+                      {formState.errors.firstName && (
+                        <p className='text-error'>{formState.errors.firstName.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+
+              <div className='pb-4'>
+                <label className='label'>Apellido Paterno</label>
+                <Controller
+                  name='first_lastname'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'El apellido paterno es requerido'
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='text'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Apellido Paterno'
+                      />
+                      {formState.errors.firstLastName && (
+                        <p className='text-error'>{formState.errors.firstLastName.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+
+              <div className='pb-4'>
+                <label className='label'>Apellido Materno</label>
+                <Controller
+                  name='second_lastname'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'El apellido materno es requerido'
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='text'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Apellido Materno'
+                      />
+                      {formState.errors.secondLastName && (
+                        <p className='text-error'>{formState.errors.secondLastName.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+              <div className='pb-4'>
+                <label className='label'>Email</label>
+                <Controller
+                  name='email'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'El email es requerido',
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: 'Email inválido'
+                    }
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='text'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Email'
+                      />
+                      {formState.errors.email && (
+                        <p className='text-error'>{formState.errors.email.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+
+              {/* <div className='pb-4'>
+                <label className='label'>Confirmar Email</label>
+                <Controller
+                  name='confirmEmail'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'La confirmación de email es requerida',
+                    validate: (value) => value === watch('email') || 'Los emails no coinciden'
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='text'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Confirmar Email'
+                      />
+                      {formState.errors.confirmEmail && (
+                        <p className='text-error'>{formState.errors.confirmEmail.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div> */}
+
+              <div className='pb-4'>
+                <label className='label'>Contraseña</label>
+                <Controller
+                  name='password'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'La contraseña es requerida',
+                    minLength: {
+                      value: 6,
+                      message: 'La contraseña debe tener al menos 6 caracteres'
+                    }
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='password'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Contraseña'
+                      />
+                      {formState.errors.password && (
+                        <p className='text-error'>{formState.errors.password.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+
+              <div className='pb-4'>
+                <label className='label'>Confirmar Contraseña</label>
+                <Controller
+                  name='confirmPassword'
+                  control={control}
+                  defaultValue=''
+                  rules={{
+                    required: 'La confirmación de contraseña es requerida',
+                    validate: (value) =>
+                      value === watch('password') || 'Las contraseñas no coinciden'
+                  }}
+                  render={({ field }) => (
+                    <>
+                      <input
+                        {...field}
+                        type='password'
+                        className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
+                        placeholder='Confirmar Contraseña'
+                      />
+                      {formState.errors.confirmPassword && (
+                        <p className='text-error'>{formState.errors.confirmPassword.message}</p>
+                      )}
+                    </>
+                  )}
+                />
+              </div>
+
+              <button
+                type='submit'
+                className='btn border-none mt-4 rounded-2xl bg-d-dark-dark-purple text-d-white hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple disabled:text-d-white'
               >
-                <option value='Tipo de perfil'>Tipo de perfil</option>
-                <option value='admin'>Admin</option>
-                <option value='restock'>Reponedor</option>
-                <option value='read'>Lector</option>
-              </select>
+                Crear cuenta
+              </button>
+            </form>
 
-            </div>
-            <label className='label' />
-            <span className='label-text'>Email</span>
-            <input
-              type='text'
-              className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
-              name='email'
-              placeholder='Email'
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              required
-            />
-
-            <label className='label' />
-            <span className='label-text'>Contraseña</span>
-            <input
-              type='password'
-              className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
-              name='password'
-              placeholder='Contraseña'
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              minLength='6'
-              required
-            />
-
-            <label className='label' />
-            <span className='label-text'>Confirma contraseña</span>
-            <input
-              type='password'
-              className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
-              name='confirm_password'
-              placeholder='Confirma contraseña'
-              required
-            />
-            <label className='label' />
-            <span className='label-text'>Nombre</span>
-
-            <input
-              type='text'
-              className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
-              name='name'
-              placeholder='Nombre'
-              required
-              value={user.first_name}
-              onChange={(e) => setUser({ ...user, first_name: e.target.value })}
-            />
-            <label className='label' />
-            <span className='label-text'>Apellido paterno</span>
-            <input
-              type='text'
-              className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
-              placeholder='Apellido paterno'
-              required
-              value={user.first_lastname}
-              onChange={(e) => setUser({ ...user, first_lastname: e.target.value })}
-            />
-            <label className='label' />
-            <span className='label-text'>Apellido materno</span>
-            <input
-              type='text'
-              className='input input-bordered w-full bg-d-white rounded-full text-d-dark-dark-purple'
-              placeholder='Apellido materno'
-              required
-              value={user.second_lastname}
-              onChange={(e) => setUser({ ...user, second_lastname: e.target.value })}
-            />
-
-            <button
-              type='button'
-              className='btn border-none mt-4 rounded-2xl bg-d-dark-dark-purple text-d-white hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple disabled:text-d-white'
-              onClick={handleCreateAccount}
-            >Crear cuenta
-            </button>
-            {error && <p className='p-2 text-error my-4'>{error}</p>}
           </div>
           <div>
             {successMessage &&
@@ -138,7 +265,6 @@ function CreateUserForm () {
           </div>
         </div>
       </div>
-
     </>
   )
 }
---
+++ src/pages/users/list.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import Link from 'next/link'
+
+export default function UsersTable ({ data }) {
+  const { handleSubmit } = useForm()
+
+  const onSubmit = (editedUserData) => {
+    console.log(editedUserData)
+  }
+
+  if (!data || data.length === 0) {
+    return (
+      <div className='h-screen'>
+        <p className='text-center'>No hay datos disponibles</p>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <table className='table text-d-dark-dark-purple table-zebra'>
+          <thead>
+            <tr className='bg-d-dark-dark-purple text-d-white'>
+              <th />
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Rol</th>
+              <th>Editar</th>
+              <th>Borrar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user) => (
+              <tr key={user.fullname}>
+                <td />
+                <td>{user.id}</td>
+                <td>{user.fullname}</td>
+                <td>{user.role}</td>
+                <td>
+
+                  <Link href='/users/edit/'>
+                    <p>✎</p>
+                  </Link>
+                </td>
+                <td>🗑</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div />
+      </form>
+    </>
+  )
+}
