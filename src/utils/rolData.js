e9dadf4982426571c981bf3aa5df93b80a30ea29
ColoradaGreis <graciana.baratti@gmail.com>
2023-09-26 16:33:58 -0300

    [feat] add json

---
+++ src/utils/rolData.js
@@ -0,0 +1,26 @@
+export const accessData = [
+  {
+    role: 'Admin',
+    menuItems_label: [
+      'Inventario',
+      'Reposiciones',
+      'Tiendas',
+      'Usuarios',
+      'Tareas'
+    ]
+  },
+  {
+    role: 'restock',
+    menuItems_label: [
+      'Reposiciones',
+      'Tiendas',
+      'Tareas'
+    ],
+    submenu_pages: [
+      'Reponer tienda',
+      'Stock actual productos',
+      'Stock por maquina',
+      'Listado Tareas'
+    ]
+  }
+]
