85d4d29b1d8138b171c77e8cbb6c4db03047682f
ColoradaGreis <graciana.baratti@gmail.com>
2023-11-13 15:17:11 -0300

    [feat] add incons

--- src/app/restock/cameraModal.js
+++ src/app/restock/cameraModal.js
@@ -14,22 +14,41 @@ function CameraModal ({
   handleConfirmationModal,
   takeSnapshot
 }) {
-  const [image, setImage] = useState(null)
-  const [showcamera, setShowCamera] = useState(true)
+  const [image, setImage] = useState('')
+  const [showcamera, setShowCamera] = useState(false)
+  const [showInput, setShowInput] = useState(false)
   const [selectedFile, setSelectedFile] = useState(null)
+  const isFullscreen = false
+  const cameraRef = React.useRef()
 
   const handleTakePhoto = (dataUri) => {
     // Callback cuando se toma una foto
     setImage(dataUri)
     setShowCamera(false)
   }
-  const handleRetakePhoto = () => {
-    setImage(null)
-    setShowCamera(true)
+  const handleShowCamera = () => {
+    setShowCamera(!showcamera)
   }
-  const sendTakenPhoto = () => {
-    takeSnapshot(image)
-    handleConfirmationModal()
+  const handleShowInputFile = () => {
+    setShowCamera(false)
+    setShowInput(true)
+  }
+
+  function handleTakePhotoAnimationDone (dataUri) {
+    console.log(dataUri, 'takePhoto')
+    setImage(dataUri)
+  }
+
+  function handleCameraError (error) {
+    console.log('handleCameraError', error)
+  }
+
+  function handleCameraStart (stream) {
+    console.log('handleCameraStart')
+  }
+
+  function handleCameraStop () {
+    console.log('handleCameraStop')
   }
   const handleFileInputChange = (e) => {
     const file = e.target.files[0]
@@ -49,13 +68,6 @@ function CameraModal ({
     <div>
       <div id='YOUR_ID' className='fixed z-50 inset-0 overflow-y-auto'>
         <div className='flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
-
-          <div className='fixed inset-0 transition-opacity' aria-hidden='true'>
-            <div className='absolute inset-0 bg-gray-500 opacity-75' />
-          </div>
-
-          <span className='hidden sm:inline-block sm:align-middle sm:h-screen' aria-hidden='true'>&#8203;</span>
-
           <div
             className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6'
             role='dialog' aria-modal='true' aria-labelledby='modal-headline'
@@ -78,32 +90,81 @@ function CameraModal ({
                 <h3 className='text-lg leading-6 font-medium text-gray-900' id='modal-headline'>
                   {title}
                 </h3>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>
-                    {message}
-                  </p>
-                </div>
               </div>
             </div>
+
             <div className='mt-5'>
-              <div className={`${showcamera ? 'mt-5' : 'hidden'}`}>
-                <Camera
-                  onTakePhoto={(dataUri) => { handleTakePhoto(dataUri) }}
-                  idealFacingMode='environment'
-                />
-              </div>
-              <div className={`${!showcamera ? 'mt-5 mb-5' : 'hidden'}`}>
-                <img src={image} alt='Taken photo' />
-                <button type='button' className='w-full mb-5 inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-d-dark-dark-purple font-medium hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple text-white sm:ml-3 sm:w-auto sm:text-sm' onClick={handleRetakePhoto}>Tomar Otra Foto</button>
-                <input type='file' accept='image/*' onChange={handleFileInputChange} />
-                <button type='button' className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-d-dark-dark-purple font-medium hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple text-white sm:ml-3 sm:w-auto sm:text-sm' onClick={sendTakenPhoto}>
-                  {confirmButtonText}
-                </button>
-              </div>
+              {image
+                ? (
+                  <div className='relative'>
+                    <button className='absolute top-2 right-2 z-10' onClick={handleShowCamera}>
+                      {/* Puedes personalizar el botón de cerrar según tus necesidades */}
+                      <span className='text-2xl font-bold text-grey-700'>X</span>
+                    </button>
+                    <img src={image} alt='Taken photo' />
+                  </div>
+                  )
+                : showcamera
+                  ? (
+                    <div className={`${showcamera ? 'mt-2 relative' : 'hidden'}`}>
+                      <button className='absolute top-2 right-2 z-10' onClick={handleShowCamera}>
+                        {/* Puedes personalizar el botón de cerrar según tus necesidades */}
+                        <span className='text-2xl font-bold text-white'>X</span>
+                      </button>
+                      <Camera
+                        className='z-0'
+                        ref={cameraRef}
+                        onTakePhoto={(dataUri) => {
+                          handleTakePhoto(dataUri)
+                        }}
+                        onTakePhotoAnimationDone={(dataUri) => {
+                          handleTakePhotoAnimationDone(dataUri)
+                        }}
+                        onCameraError={(error) => {
+                          handleCameraError(error)
+                        }}
+                        onCameraStart={(stream) => {
+                          handleCameraStart(stream)
+                        }}
+                        onCameraStop={() => {
+                          handleCameraStop()
+                        }}
+                      />
+                    </div>
+                    )
+                  : !showInput
+                      ? (
+                        <div className='flex space-x-4'>
+                          <button className='flex-grow' onClick={handleShowCamera}>
+                            <div className='flex-grow'>
+                              <div className='flex flex-col justify-center items-center w-full h-full rounded-lg border border-gray-300 p-4 text-center'>
+                                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'>
+                                  <path stroke-linecap='round' stroke-linejoin='round' d='M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z' />
+                                  <path stroke-linecap='round' stroke-linejoin='round' d='M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z' />
+                                </svg>
+                                <p className='text-xs'>Tomar foto</p>
+                              </div>
+                            </div>
+                          </button>
+                          <button className='flex-grow' onClick={handleShowInputFile}>
+                            <div className='flex-grow'>
+                              <div className='flex flex-col justify-center items-center w-full h-full rounded-lg border border-gray-300 p-4 text-center'>
+                                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6'>
+                                  <path stroke-linecap='round' stroke-linejoin='round' d='M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z' />
+                                </svg>
+                                <p className='text-xs'>Subir imagen</p>
+                              </div>
+                            </div>
+                          </button>
+
+                        </div>
+                        )
+                      : (
+                        <input type='file' accept='image/*' onChange={handleFileInputChange} />
+                        )}
             </div>
 
             <div className='mt-5 sm:mt-4 sm:flex sm:flex-row-reverse'>
-
               <button type='button' databehavior='cancel' className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm' onClick={handleOperationConfirmation}>
                 {cancelButtonText}
               </button>
