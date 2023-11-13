35a0dec7fa34e60b8d8fbec24b62d6ba13c7dac2
ColoradaGreis <graciana.baratti@gmail.com>
2023-11-13 15:30:36 -0300

    [fix] add html image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['despnsa247-public-files.s3.amazonaws.com']
+  }
 }
 
 module.exports = nextConfig
