c1c7ef48c60a73c6ffa4d9144f1842922d73504d
lalailamas <ale.cordoba.p@gmail.com>
2023-11-14 10:49:52 -0300

    Co-authored-by: Graciana Baratti <ColoradaGreis@users.noreply.github.com>

--- src/components/admin/common/datepicker/double.js
+++ src/components/admin/common/datepicker/double.js
@@ -1,6 +1,28 @@
 import Datepicker from 'react-tailwindcss-datepicker'
+import dayjs from 'dayjs'
 
 function DatePicker ({ startDate, endDate, handleDateChange }) {
+  // Lógica para traer información del año pasado
+  // const getLastYearDates = () => {
+  //   const currentDate = new Date()
+  //   const currentYear = currentDate.getFullYear()
+
+  //   const startDateLastYear = `01-01-${currentYear - 1}`
+  //   console.log(startDateLastYear, 'inicio año pasado')
+  //   const endDateLastYear = `31-12-${currentYear - 1}`
+  //   console.log(endDateLastYear, 'final año pasado')
+
+  //   return { startDate: startDateLastYear, endDate: endDateLastYear }
+  // }
+  const getLastYearDates = () => {
+    // const currentYear = dayjs().year()
+    const lastYearStartDate = dayjs().subtract(1, 'year').startOf('year').format('DD-MM-YYYY')
+    console.log(lastYearStartDate, 'inicio año pasado')
+    const lastYearEndDate = dayjs().subtract(1, 'year').endOf('year').format('DD-MM-YYYY')
+    console.log(lastYearEndDate, 'fin año pasado')
+
+    return { startDate: lastYearStartDate, endDate: lastYearEndDate }
+  }
   return (
     <div className='w-full border'>
       <Datepicker
@@ -17,7 +39,15 @@ function DatePicker ({ startDate, endDate, handleDateChange }) {
             yesterday: 'Ayer',
             past: period => `Últimos ${period} días`,
             currentMonth: 'Este mes',
-            pastMonth: 'Mes pasado'
+            pastMonth: 'Mes pasado',
+            pastYear: {
+              text: 'Año pasado',
+              period: getLastYearDates()
+              // period: {
+              //   start: getLastYearDates().startDate,
+              //   end: getLastYearDates().endDate
+              // }
+            }
           },
           displayFormat: 'DD/MM/YYYY',
           inputClassName: 'input input-bordered w-full',
