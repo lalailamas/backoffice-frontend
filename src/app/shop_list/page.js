38bf3163b2c3240f41d806bc162da6f8e8d7a1fc
ColoradaGreis <graciana.baratti@gmail.com>
2023-11-27 14:50:53 -0300

    [feat] add shop_list page

---
+++ src/app/shop_list/page.js
@@ -0,0 +1,16 @@
+import InsideLayout from '@/components/admin/layouts/inside'
+import React from 'react'
+
+function shopList () {
+  return (
+    <div>
+      <InsideLayout />
+      <div>
+        <h1>Lista de compras</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default shopList
