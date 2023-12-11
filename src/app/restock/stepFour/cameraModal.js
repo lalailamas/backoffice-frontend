4290a8d61f972ec792dabab5b5d74b0f364b87c6
ColoradaGreis <graciana.baratti@gmail.com>
2023-12-11 11:59:46 -0300

    [feat] mandatory step 4 photo

--- src/app/restock/stepFour/cameraModal.js
+++ src/app/restock/stepFour/cameraModal.js
@@ -210,7 +210,7 @@ function CameraModal ({
             </div>
 
             <div className='mt-5 sm:mt-4 sm:flex justify-start sm:flex-row-reverse justify-start'>
-              <div className={`${image ? 'hidden' : ''}`}>
+              {/* <div className={`${image ? 'hidden' : ''}`}>
                 <button
                   type='button'
                   databehavior='submit'
@@ -219,7 +219,7 @@ function CameraModal ({
                 >
                   Siguiente
                 </button>
-              </div>
+              </div> */}
 
               <button type='button' databehavior='cancel' className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm' onClick={handleOperationConfirmation}>
                 {cancelButtonText}
