f812e0e22e5e407ae3ac44604183e809675d0ceb
ColoradaGreis <graciana.baratti@gmail.com>
2023-12-12 15:24:34 -0300

    [feat] create handleError

--- src/utils/errors/errors.js
+++ src/utils/errors/errors.js
@@ -2,29 +2,39 @@
 import { useRouter } from 'next/navigation'
 import { swallError } from '../sweetAlerts'
 import * as Sentry from '@sentry/nextjs'
+import { withScope } from '@sentry/nextjs'
 
 export const errorHandler = (error, route, data) => {
   withScope((scope) => {
     // Agrega información adicional al contexto de Sentry
-    scope.setContext('Stock Data', data)
+    scope.setContext('Data', data)
 
     if (error instanceof ValidationError) {
       swallError(error.message, false)
     } else if (error instanceof ConnectionError) {
       swallError(error.message, false)
     } else if (error.response && error.response.status === 403) {
+      // Captura la excepción en Sentry con el contexto adicional
+      Sentry.captureException(error)
       swallError('La transacción ya fue validada', false)
       useRouter.push(route)
     } else if (error.response && error.response.status === 400) {
+      // Captura la excepción en Sentry con el contexto adicional
+      Sentry.captureException(error)
       swallError('Solicitud incorrecta', false)
     } else if (error.response && error.response.status === 500) {
+      // Captura la excepción en Sentry con el contexto adicional
+      Sentry.captureException(error)
       swallError('Error en el servidor', false)
+    } else if (error.response && error.response.status === 422) {
+      // Captura la excepción en Sentry con el contexto adicional
+      Sentry.captureException(error)
+      swallError('La solicitud no se pudo procesar', false)
     } else {
+      // Captura la excepción en Sentry con el contexto adicional
+      Sentry.captureException(error)
       swallError('Error desconocido', false)
     }
-
-    // Captura la excepción en Sentry con el contexto adicional
-    Sentry.captureException(error)
   })
 }
 
