cb61c813ce19574751cf66b5da70ff8e3c81d362
lalailamas <ale.cordoba.p@gmail.com>
2024-02-09 11:49:09 -0300

    [feat] add footer & fix pager bugsillo

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Sidebar from '@/components/admin/common/sidebar'
 import Navbar from '@/components/admin/common/navbar'
 import { useState } from 'react'
 import { usePathname } from 'next/navigation'
+import Footer from '@/components/admin/common/footer'
 
 export default function RootLayout ({ children }) {
   const [showDrawer, setShowDrawer] = useState(false)
@@ -15,7 +16,7 @@ export default function RootLayout ({ children }) {
     <html lang='es'>
       <body data-theme='light' className='bg-d-white'>
         <SessionAuthProvider>
-          <div className='drawer-content overflow-x-auto h-screen bg-d-white'>
+          <div className='drawer-content overflow-x-auto flex flex-col min-h-screen bg-d-white'>
             <div className='drawer' data-theme='light'>
               <input
                 id='dsp-drawer'
@@ -34,9 +35,14 @@ export default function RootLayout ({ children }) {
                 {!isLoginPage && showDrawer && <Sidebar toggle={() => setShowDrawer(!showDrawer)} />}
               </div>
             </div>
+            <div className='flex-grow'>
 
-            {children}
+              {children}
+            </div>
 
+            <div className='mt-auto drawer-content overflow-x-auto bg-d-white '>
+              <Footer />
+            </div>
           </div>
         </SessionAuthProvider>
       </body>
