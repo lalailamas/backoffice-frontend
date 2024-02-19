2260fcf7a9f5f080d21674e58b65234e9ac11cf3
ColoradaGreis <graciana.baratti@gmail.com>
2024-02-19 17:46:53 -0300

    [refactor] change to ButtonCancel in simpleModal

--- src/components/admin/modals/simpleModal.js
+++ src/components/admin/modals/simpleModal.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ButtonCancel from '../common/buttons/ButtonCancel'
 
 function SimpleModal ({ title, message, cancelButtonText, handleConfirmationModal }) {
   return (
@@ -37,10 +38,7 @@ function SimpleModal ({ title, message, cancelButtonText, handleConfirmationModa
               </div>
             </div>
             <div className='mt-5 sm:mt-4 sm:flex sm:flex-row-reverse'>
-
-              <button type='button' databehavior='cancel' className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm' onClick={handleConfirmationModal}>
-                {cancelButtonText}
-              </button>
+              <ButtonCancel onClick={handleConfirmationModal} />
             </div>
           </div>
         </div>
