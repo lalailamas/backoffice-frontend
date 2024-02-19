3e8725aee484d1678f2acc69beef88e177a7696a
ColoradaGreis <graciana.baratti@gmail.com>
2024-02-19 17:11:17 -0300

    [refactor] change button with ButtonPrimary in stock adjustment

--- src/app/stock_adjustment/page.js
+++ src/app/stock_adjustment/page.js
@@ -9,6 +9,7 @@ import FileSaver from 'file-saver'
 import { swallError, Toast, swallInfo } from '@/utils/sweetAlerts'
 import Swal from 'sweetalert2'
 import PriceModal from '@/components/admin/modals/priceModal'
+import ButtonPrimary from '@/components/admin/common/buttons/ButtonPrimary'
 
 function StockAdjustment () {
   const { stores } = useGetStores2()
@@ -163,12 +164,7 @@ function StockAdjustment () {
           </select>
         </div>
         <div className={`${selectedStore ? 'flex justify-end p-5' : 'hidden'}`}>
-          <button onClick={handleExcelDownload} className='btn btn-sm join-item rounded-full bg-d-dark-dark-purple border-none text-d-white hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple'>
-            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6 mr-2'>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3' />
-            </svg>
-            Descargar
-          </button>
+          <ButtonPrimary text='Descargar' onClick={handleExcelDownload} type='download' />
         </div>
 
       </div>
