80213f3266afcd286fd31551ffe16a64883eea2a
ColoradaGreis <graciana.baratti@gmail.com>
2024-02-19 17:46:42 -0300

    [refactor] change to ButtonCancel in quantityModal

--- src/components/admin/modals/quantityModal.js
+++ src/components/admin/modals/quantityModal.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { putStockInventory } from '@/api/stock'
+import ButtonCancel from '../common/buttons/ButtonCancel'
 
 function QuantityModal ({
   handleOperationConfirmation,
@@ -123,9 +124,8 @@ function QuantityModal ({
               <button type='button' className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-d-dark-dark-purple font-medium hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple text-white sm:ml-3 sm:w-auto sm:text-sm' onClick={quantityChangeHandler}>
                 {confirmButtonText}
               </button>
-              <button type='button' databehavior='cancel' className='mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:w-auto sm:text-sm' onClick={handleConfirmationModal}>
-                {cancelButtonText}
-              </button>
+
+              <ButtonCancel onClick={handleConfirmationModal} />
             </div>
 
           </div>
