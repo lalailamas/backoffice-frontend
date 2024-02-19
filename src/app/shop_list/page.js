83bf3fb7e56b6fc073bdc9375e2727e87c005c97
ColoradaGreis <graciana.baratti@gmail.com>
2024-02-19 17:10:53 -0300

    [refactor] change button with ButtonPrimary in shop_list

--- src/app/shop_list/page.js
+++ src/app/shop_list/page.js
@@ -10,6 +10,7 @@ import { swallError, Toast } from '@/utils/sweetAlerts'
 import Swal from 'sweetalert2'
 import TabsComponent from '@/components/admin/common/tabs'
 import RequestedStockTable from './requestedStockTable'
+import ButtonPrimary from '@/components/admin/common/buttons/ButtonPrimary'
 
 function shopList () {
   const [ids, setIds] = useState([])
@@ -190,20 +191,10 @@ function shopList () {
           <div className='flex justify-end gap-4'>
 
             <div className=''>
-              <button onClick={handleExcelDownloadRequested} className='btn btn-sm join-item rounded-full bg-d-dark-dark-purple border-none text-d-white hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple'>
-                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6 mr-2'>
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3' />
-                </svg>
-                Descargar Reposición por tienda
-              </button>
+              <ButtonPrimary text='Descargar Reposición por tienda' onClick={handleExcelDownloadRequested} type='download' />
             </div>
             <div className=''>
-              <button onClick={handleExcelDownload} className='btn btn-sm join-item rounded-full bg-d-dark-dark-purple border-none text-d-white hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple'>
-                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6 mr-2'>
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3' />
-                </svg>
-                Descargar Lista de compras
-              </button>
+              <ButtonPrimary text='Descargar Lista de compras' onClick={handleExcelDownload} type='download' />
             </div>
           </div>}
       </div>
