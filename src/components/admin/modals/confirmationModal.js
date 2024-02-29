c26066373b05325c7b0e6305231d40d5d9892610
ColoradaGreis <graciana.baratti@gmail.com>
2024-02-29 14:53:50 -0300

    [fix] fix modal import

--- src/components/admin/modals/confirmationModal.js
+++ src/components/admin/modals/confirmationModal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ButtonCancel from '../common/buttons/ButtonCancel'
 import ButtonPrimary from '../common/buttons/ButtonPrimary'
-import ButtonPrimary from '../common/buttons/ButtonPrimary'
 
 function ConfirmationModal ({ handleOperationConfirmation, title, message, confirmButtonText, cancelButtonText, handleConfirmationModal }) {
   return (
