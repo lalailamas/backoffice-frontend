0468d585752a05b8415dcfb18b4e5f36fb3f4392
ColoradaGreis <graciana.baratti@gmail.com>
2024-03-07 14:42:48 -0300

    [fix] add text-xs to buttons

--- src/components/admin/common/buttons/ButtonCancel.js
+++ src/components/admin/common/buttons/ButtonCancel.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ButtonCancel ({ onClick, text = 'Cancelar' }) {
   return (
-    <button type='button' databehavior='cancel' className='btn join-item rounded-full' onClick={onClick}>
+    <button type='button' databehavior='cancel' className='btn text-xs join-item rounded-full' onClick={onClick}>
       {text}
     </button>
   )
