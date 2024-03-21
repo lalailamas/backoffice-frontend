8fbd7fa482f010e3df9f8880f6f32c7aa28a2b08
ColoradaGreis <graciana.baratti@gmail.com>
2024-03-21 12:12:23 -0300

    [feat] delete shopList button

--- src/app/(private)/shop_list/page.js
+++ src/app/(private)/shop_list/page.js
@@ -1,46 +1,26 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import FileSaver from 'file-saver'
-import { downloadShopList, downloadStoresStock, getRepositionByStore, getShopList } from '@/api/stock'
+import { downloadStoresStock, getRepositionByStore } from '@/api/stock'
 import useGetStores2 from '@/hooks/useStores2'
 import { MultiSelect, MultiSelectItem } from '@tremor/react'
-import ShopListTable from './shopListTable'
-import DspLoader from '@/components/admin/common/loader'
 import { swallError, Toast } from '@/utils/sweetAlerts'
 import Swal from 'sweetalert2'
-import TabsComponent from '@/components/admin/common/tabs'
 import RequestedStockTable from './requestedStockTable'
 import ButtonPrimary from '@/components/admin/common/buttons/ButtonPrimary'
 import MainTitle from '@/components/admin/common/titles/MainTitle'
+import DspLoader from '@/components/admin/common/loader'
 
 function shopList () {
   const [ids, setIds] = useState([])
   const { stores } = useGetStores2(false)
-  const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [dataRequested, setDataRequested] = useState([])
-  const [loadingRequested, setLoadingRequested] = useState(false)
-  console.log(stores, 'stores')
+  // console.log(stores, 'stores')
   const handleStoreChange = (selectedIds) => {
     setIds(selectedIds)
   }
 
-  const handleExcelDownload = async () => {
-    Toast('Descargando archivo', 'espera unos segundos', 2000)
-    try {
-      const response = await downloadShopList(ids)
-      // console.log(response.data, 'response')
-      const { buffer, filename } = response.data
-
-      const blob = new Blob([Buffer.from(buffer)], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })
-
-      FileSaver.saveAs(blob, filename)
-      Swal.close()
-    } catch (error) {
-      console.error('Error al descargar el archivo Excel:', error)
-      Swal.close()
-    }
-  }
   const handleShopList = async () => {
     setLoading(true)
 
@@ -50,13 +30,12 @@ function shopList () {
       return
     }
     try {
-      const response = await getShopList(ids)
-      handleShopListRequested()
+      const response = await getRepositionByStore(ids)
       if (response.length === 0) {
         swallError('No hay datos para mostrar', false)
       } else {
         setLoading(false)
-        setData(response)
+        setDataRequested(response)
       }
     } catch (error) {
       swallError('Error al obtener los datos', false)
@@ -79,74 +58,10 @@ function shopList () {
       Swal.close()
     }
   }
-  const handleShopListRequested = async () => {
-    setLoadingRequested(true)
-    try {
-      const responseRequested = await getRepositionByStore(ids)
-      console.log(responseRequested, 'response.data')
-      if (responseRequested.length === 0) {
-        swallError('No hay datos para mostrar', false)
-      } else {
-        setLoadingRequested(false)
-        setDataRequested(responseRequested)
-      }
-    } catch (error) {
-      swallError('Error al obtener los datos', false)
-      setLoadingRequested(false)
-      console.error('Error al obtener los datos', error)
-    }
-  }
   useEffect(() => {
     console.log(ids, 'ids')
   }, [ids])
 
-  const tabs = [
-    {
-      id: 'shopList',
-      name: 'Lista de compras',
-      active: true,
-      content: (
-        <div className='justify-center'>
-          {(loading)
-            ? (
-              <DspLoader />
-              )
-            : (
-              <div className=''>
-                <ShopListTable data={data} />
-              </div>
-              )}
-
-          {/* <div><pre>{JSON.stringify(ids, null, 2)}</pre></div> */}
-        </div>
-
-      )
-    },
-    {
-      id: 'RepositionByStore',
-      name: 'Reposición por tienda',
-      active: false,
-      content: (
-        <div className=''>
-
-          {
-        (loadingRequested)
-          ? (<DspLoader />)
-          : (
-            <div>
-
-              <div>
-                <RequestedStockTable data={dataRequested} />
-              </div>
-            </div>
-            )
-  }
-        </div>
-
-      )
-
-    }
-  ]
   return (
     <div className='h-screen'>
       <MainTitle>Gestión de abastecimiento</MainTitle>
@@ -190,14 +105,19 @@ function shopList () {
             <div className=''>
               <ButtonPrimary text='Descargar Reposición por tienda' onClick={handleExcelDownloadRequested} type='download' />
             </div>
-            <div className=''>
-              <ButtonPrimary text='Descargar Lista de compras' onClick={handleExcelDownload} type='download' />
-            </div>
           </div>}
       </div>
-      <div className='p-10'>
-        <TabsComponent tabs={tabs} />
-      </div>
+      {loading
+        ? <DspLoader />
+        : (
+          <div className='p-10'>
+            <div>
+
+              <div>
+                <RequestedStockTable data={dataRequested} />
+              </div>
+            </div>
+          </div>)}
     </div>
 
   )
