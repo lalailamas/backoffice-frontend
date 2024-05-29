71304659fad5387fdc39e0fbb62ac0a630bc940b
ColoradaGreis <graciana.baratti@gmail.com>
2024-05-29 13:32:41 -0300

    Update environment variables and next.config.js for NEXTAUTH_URL configuration

--- next.config.js
+++ next.config.js
@@ -2,13 +2,17 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 console.log('NODE_ENV:', process.env.NODE_ENV)
+console.log('NEXTAUTH_URL_DEV:', process.env.NEXTAUTH_URL_DEV)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['despnsa247-public-files.s3.amazonaws.com']
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NODE_ENV === 'development' ? process.env.NEXTAUTH_URL_DEV : process.env.NEXTAUTH_URL_PROD
   }
 }
-
+console.log('NEXTAUTH_URL:', nextConfig.env.NEXTAUTH_URL)
 module.exports = nextConfig
