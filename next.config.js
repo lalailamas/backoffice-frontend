ca5d8684b8228209c5f7e9a7c754b6efa577fc11
ColoradaGreis <graciana.baratti@gmail.com>
2024-05-29 13:28:34 -0300

    chore: Update environment variables in .env.development and .env.local files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+console.log('NODE_ENV:', process.env.NODE_ENV)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['despnsa247-public-files.s3.amazonaws.com']
-  },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL
   }
 }
 
