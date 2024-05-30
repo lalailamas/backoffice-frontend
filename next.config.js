5b5d856e7d1457d1b490be2131baba4cf04e5859
ColoradaGreis <graciana.baratti@gmail.com>
2024-05-30 15:37:14 -0300

    chore: Update NEXT_PUBLIC_DSP_API_BASE in .env.development, .env.local, and .env.local.template files

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-console.log('NODE_ENV:', process.env.NODE_ENV)
-console.log('NEXTAUTH_URL:', process.env.NEXTAUTH_URL)
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['despnsa247-public-files.s3.amazonaws.com']
-  },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL
   }
 }
 module.exports = nextConfig
