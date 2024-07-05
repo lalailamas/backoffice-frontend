007c1d946f7c965974fb59e25216de7de9c090d4
lalailamas <ale.cordoba.p@gmail.com>
2024-07-05 12:49:57 -0400

    [feat] add layout history, pending fix format date

---
+++ src/app/(private)/stores/detail/layoutHistory.js
@@ -0,0 +1,162 @@
+import { getLayoutHistory, getAllLayouts } from '@/api/layout'
+import React, { useEffect, useState } from 'react'
+import { swallError, swallInfo } from '@/utils/sweetAlerts'
+import DatePicker from '@/components/admin/common/datepicker/double'
+import dayjs from 'dayjs'
+
+function LayoutHistory ({ storeId, products }) {
+  const [layouts, setLayouts] = useState([])
+  const [allLayouts, setAllLayouts] = useState({})
+  const [selectedProducts, setSelectedProducts] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [dateRange, setDateRange] = useState({
+    startDate: dayjs().subtract(1, 'month').startOf('month').toDate(),
+    endDate: dayjs().subtract(1, 'month').endOf('month').toDate()
+  })
+
+  const productMap = products.reduce((map, product) => {
+    map[product.productId] = product.productName
+    return map
+  }, {})
+
+  useEffect(() => {
+    const fetchAllLayouts = async () => {
+      try {
+        const response = await getAllLayouts()
+        const layoutMap = response.reduce((map, layout) => {
+          map[layout.id] = layout.name
+          return map
+        }, {})
+        setAllLayouts(layoutMap)
+      } catch (error) {
+        swallError('Error al obtener todos los layouts', false)
+      }
+    }
+
+    fetchAllLayouts()
+  }, [])
+
+  useEffect(() => {
+    if (dateRange.startDate !== null && dateRange.endDate !== null) {
+      const formattedStartDate = dayjs(dateRange.startDate).format('YYYY-MM-DD')
+      const formattedEndDate = dayjs(dateRange.endDate).format('YYYY-MM-DD')
+      console.log('Fetching layout history with dates:', formattedStartDate, formattedEndDate)
+      getLayoutHistory(storeId, formattedStartDate, formattedEndDate, 1, 1000)
+        .then((response) => {
+          console.log(response.rows.length, 'respuesta')
+          if (response.rows.length === 0) {
+            swallInfo('No hay datos para el rango de fechas seleccionado')
+          }
+          setLayouts(response.rows)
+        })
+        .catch((error) => {
+          swallError('Error al obtener el historial de layout', false)
+          console.error('Error fetching layout history:', error)
+        })
+    }
+  }, [dateRange])
+
+  const openModal = (products) => {
+    const productNames = products.map((productId) => productMap[productId] || productId)
+    productNames.sort()
+    setSelectedProducts(productNames)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setSelectedProducts([])
+  }
+
+  const handleDateChange = (newDateRange) => {
+    const formattedStartDate = dayjs(newDateRange.startDate).format('YYYY-MM-DD')
+    const formattedEndDate = dayjs(newDateRange.endDate).format('YYYY-MM-DD')
+    console.log('New date range selected:', { startDate: formattedStartDate, endDate: formattedEndDate })
+    setLayouts([]) // Reset data
+    setDateRange({ startDate: formattedStartDate, endDate: formattedEndDate })
+  }
+
+  return (
+    <div className='overflow-x-auto'>
+      <div className='w-full px-10 mt-2'>
+        <DatePicker
+          startDate={dateRange.startDate}
+          endDate={dateRange.endDate}
+          handleDateChange={handleDateChange}
+        />
+      </div>
+      <div className='p-2'>
+        <table className='table text-d-dark-dark-purple table-zebra mt-8 p-8'>
+          <thead>
+            <tr className='bg-d-dark-dark-purple text-d-white'>
+              <th className='px-4 py-2'>ID</th>
+              <th className='px-4 py-2'>Store ID</th>
+              <th className='px-4 py-2'>Nombre de Layout</th>
+              <th className='px-4 py-2'>Fecha de Inicio</th>
+              <th className='px-4 py-2'>Fecha de Término</th>
+              <th className='px-4 py-2'>En Transición</th>
+              <th className='px-4 py-2'>Productos</th>
+              <th className='px-4 py-2'>Fecha de Creación</th>
+            </tr>
+          </thead>
+          <tbody>
+            {layouts.length > 0
+              ? (
+                  layouts.map((item) => (
+                    <tr key={item.id}>
+                      <td className='border px-4 py-2'>{item.id}</td>
+                      <td className='border px-4 py-2'>{item.store_id}</td>
+                      <td className='border px-4 py-2'>{allLayouts[item.layout_id] || item.layout_id}</td>
+                      <td className='border px-4 py-2'>{dayjs(item.start_date).format('DD/MM/YYYY')}</td>
+                      <td className='border px-4 py-2'>{item.end_date ? dayjs(item.end_date).format('DD/MM/YYYY') : 'N/A'}</td>
+                      <td className='border px-4 py-2'>{item.is_transition ? 'Sí' : 'No'}</td>
+                      <td className='border px-4 py-2'>
+                        <button
+                          className='btn rounded-full bg-d-dark-dark-purple border-none text-d-white hover:bg-d-soft-soft-purple hover:text-d-dark-dark-purple'
+                          onClick={() => openModal(item.products_id)}
+                        >
+                          Ver Detalle
+                        </button>
+                      </td>
+                      <td className='border px-4 py-2'>{dayjs(item.createdAt).format('DD/MM/YYYY')}</td>
+                    </tr>
+                  ))
+                )
+              : (
+                <tr>
+                  <td className='border px-4 py-2' colSpan='8'>
+                    No hay historial de layout disponible
+                  </td>
+                </tr>
+                )}
+          </tbody>
+        </table>
+      </div>
+
+      {isModalOpen && (
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+          <div className='bg-white p-4 rounded'>
+            <h2 className='text-xl mb-4 font-bold'>Productos</h2>
+            <ul className='list-disc list-inside'>
+              {selectedProducts.map((product, index) => (
+                <li key={index}>{product}</li>
+              ))}
+            </ul>
+            <div className='p-2 flex justify-center'>
+              <button
+                type='button'
+                databehavior='cancel'
+                className='btn text-xs join-item rounded-full'
+                onClick={closeModal}
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default LayoutHistory
