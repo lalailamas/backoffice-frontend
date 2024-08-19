6950c404bdbdc4b124dc65684a1f3a03dd8849b0
lalailamas <ale.cordoba.p@gmail.com>
2024-08-19 12:34:03 -0400

    [feat] add stores links with GA & GTM

--- src/app/(public)/links/hernan_cortes/page.js
+++ src/app/(public)/links/hernan_cortes/page.js
@@ -5,7 +5,8 @@ export default function HernanCortesStore () {
     <StoreLinks
       catalogHref='https://app.despnsa247.com/catalog/CNV_008'
       catalogText='Catálogo Hernán Cortés'
-
+      gtmId={process.env.NEXT_PUBLIC_HERNAN_CORTES_GTM_ID}
+      gaId={process.env.NEXT_PUBLIC_HERNAN_CORTES_GA_ID}
     />
   )
 }
